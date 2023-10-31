b0f77d87cc2cb0b8bb4b03c792a337965a50208b
TImmykiller5 <106759786+TImmykiller5@users.noreply.github.com>
2023-10-31 23:58:32 +0100

    color updates

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,71 +7,87 @@ import Form3 from "./forms/Form3";
 function Homepage() {
   const [showCalc, setShowCalc] = useState(false);
   const [active, setActive] = useState(false);
-  const [activeform, setActiveForm] = useState('panel');
+  const [activeform, setActiveForm] = useState("panel");
   const handleFormSwitch = (trigger) => {
-     setActiveForm(trigger);
-  }
+    setActiveForm(trigger);
+  };
   const handleSelected = (e) => {
-    console.log(e.target)
-    setActive()
-    e.target.classList.toggle('active')
-
-  }
+    console.log(e.target);
+    setActive();
+    e.target.classList.toggle("active");
+  };
   return (
     <div className="bg-slate-50">
       <div className=" bg-slate-100 py-10 mt-16">
         <div className="flex flex-col gap-4 items-center  px-20">
-          <div className=" border-2 bg-indigo-100 border-indigo-500 text-xs rounded-2xl px-2 py-1">
+          <div className=" border-2 bg-purple-100 border-purple-500 text-xs rounded-2xl px-2 py-1">
             Reinforced Concrete Design
           </div>
           <div>
-            <h1 className="text-4xl">Flat hjsgrdo</h1>
+            <h1 className="text-4xl">WAFFLE SLAB</h1>
           </div>
           <div>
             <p className="flex text-center">
-              The Flat Slab Design app is meticulously crafted to assist
-              students and engineers in swiftly creating flat slab designs
-              across any platform. With user-friendly features and a powerful
-              design engine, you can streamline the design process, ensuring
-              efficiency and precision in every project. Welcome to a new era of
-              effortless flat slab design.
+              The waffle slab design software is engineered to ease the
+              calculations related to the design of waffle slabs and to
+              particular provide adequate reinforcement for the top slab to
+              reduce, and if possible, eliminate crack occurence.
             </p>
           </div>
           <div className="flex gap-8 items-center">
-            <button onClick={() => setShowCalc(!showCalc)} className="text-sm bg-gradient-to-b from-indigo-400 to-indigo-300 hover:from-indigo-400 hover:to-indigo-500 px-3 py-2 rounded-md text-white">
+            <button
+              onClick={() => setShowCalc(!showCalc)}
+              className="text-sm bg-gradient-to-b from-purple-400 to-purple-300 hover:from-purple-400 hover:to-purple-500 px-3 py-2 rounded-md text-white"
+            >
               Start New Project
             </button>
             <div>Or</div>
-            <button className="text-sm bg-gradient-to-b from-indigo-400 to-indigo-300 hover:from-indigo-400 hover:to-indigo-500 px-3 py-2 rounded-md text-white">
+            <button className="text-sm bg-gradient-to-b  from-purple-400 to-purple-300 hover:from-purple-400 hover:to-purple-500 px-3 py-2 rounded-md text-white">
               <p>Load Recent Project {">"}</p>
             </button>
           </div>
         </div>
       </div>
       <div className={`${showCalc ? "block" : "hidden"} `}>
-        <div  className="flex flex-col gap-8 items-center">
+        <div className="flex flex-col gap-8 items-center">
           <h1>SLAB DESIGN</h1>
           <div className="flex gap-4 justify-between w-full p-4">
             <div className={`w-1/3`}>
-              <button className="w-full" onClick={(e) => {handleFormSwitch('panel'); handleSelected(e)}}>
-              Panel Information
+              <button
+                className="w-full"
+                onClick={(e) => {
+                  handleFormSwitch("panel");
+                  handleSelected(e);
+                }}
+              >
+                Panel Information
               </button>
             </div>
             <div className={`w-1/3`}>
-              <button className="w-full" onClick={() => handleFormSwitch('reinforcement')}>
-              Reinforcement 
+              <button
+                className="w-full"
+                onClick={() => handleFormSwitch("reinforcement")}
+              >
+                Reinforcement
               </button>
             </div>
             <div className={`w-1/3`}>
-              <button className="w-full" onClick={() => handleFormSwitch('loading')}>
-              Loading Details
-
+              <button
+                className="w-full"
+                onClick={() => handleFormSwitch("loading")}
+              >
+                Loading Details
               </button>
             </div>
           </div>
           <div>
-            {activeform === 'panel' ? <Form1 /> : activeform === 'reinforcement' ? <Form2/> : <Form3/> }
-            
+            {activeform === "panel" ? (
+              <Form1 />
+            ) : activeform === "reinforcement" ? (
+              <Form2 />
+            ) : (
+              <Form3 />
+            )}
           </div>
         </div>
       </div>
